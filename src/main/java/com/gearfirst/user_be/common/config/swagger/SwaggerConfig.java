713c9719ee14ae52d64f4174f98d5bf98cbfabc2
ksoheee <thgml0716030@naver.com>
2025-11-07 19:10:57 +0900

    fix: 스웨거 url 수정

--- src/main/java/com/gearfirst/user_be/common/config/swagger/SwaggerConfig.java
+++ src/main/java/com/gearfirst/user_be/common/config/swagger/SwaggerConfig.java
@@ -12,7 +12,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         Server server = new Server();
-        server.setUrl("/");
+        server.setUrl("/user");
 
         return new OpenAPI()
                 .info(new Info()
